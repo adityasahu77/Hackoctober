0eeda763e6557e9686231bc20cbb994c0534d7e5
Aditya sahu <102940455+adityasahu77@users.noreply.github.com>
2022-11-05 19:49:36 +0530

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -1,13 +1,17 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-    Map<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) {
-        int complement = target - nums[i];
-        if (map.containsKey(complement)) {
-            return new int[] { map.get(complement), i };
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Map;
+
+class TwoSum {
+
+    // Time complexity: O(n^2)
+    private static int[] findTwoSum_BruteForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
         }
-        map.put(nums[i], i);
+        return new int[] {};
     }
-    throw new IllegalArgumentException("No two sum solution");
-}
-}
